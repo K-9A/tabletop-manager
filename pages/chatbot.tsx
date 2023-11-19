65ba148675390aaef1623cc1280b2f7cc5abd6c4
Kirill Anitpov <kirill.antipov@gmail.com>
2023-11-18 21:18:05 -0800

    Chatbot for fun

---
+++ pages/chatbot.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+
+export default function Chatbot() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Chatbot</title>
+        <meta name="Chatbot" content="Chatbot of the Tabletop Manager Website" />
+      </Head>
+      <script src="//code.tidio.co/mpa37nxhlxmwurmqqucyiw8ip59z0aeq.js" async></script>
+    </Fragment>
+  );
+}
